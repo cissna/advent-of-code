b844bd4eb328df6bc86b837b461dd969b1b5616e
Isaac Cissna <isaaccissna@gmail.com>
2025-12-23 03:01:21 -0800

    implemented notinplemented thing since ranges overlap for day5

--- day5/sparse_clumped_zeros_and_ones.py
+++ day5/sparse_clumped_zeros_and_ones.py
@@ -30,8 +30,35 @@ class SparseClumpedZerosAndOnes():
     
     def set_ones(self, start, end):
         if self.any(start, end):
-            raise NotImplementedError("this shouldn't happen in the original use case, but if this were to be completed we would want to merge the ranges probably")
-        bisect.insort(self.clumps, (start, end))
+            # 1. Find the "Leftmost" range we touch
+            # bisect_left gives insertion point. If we overlap the guy *before* that point,
+            # we need to merge with him.
+            idx1 = bisect.bisect_left(self.clumps, (start, start))
+            if idx1 > 0 and self.clumps[idx1 - 1][1] >= start:
+                idx1 -= 1  # We actually start merging from the previous clump
+
+            # 2. Find the "Rightmost" range we touch
+            # bisect_right is usually easier here to find the upper bound
+            idx2 = bisect.bisect_right(self.clumps, (end, end))
+            # If the clump at idx2 (the one after end) starts *at* end, bisect_right
+            # pushes us past it. If the clump *before* idx2 ends after 'end', we merge it.
+            if idx2 < len(self.clumps) and self.clumps[idx2][0] <= end:
+                 idx2 += 1 # Include this one too (edge case for precise boundaries)
+
+            # 3. Calculate the new Super-Range
+            # The new start is min(my_input, start_of_first_touched_clump)
+            new_start = min(start, self.clumps[idx1][0])
+            
+            # The new end is max(my_input, end_of_last_touched_clump)
+            # Note: idx2 is the slice *stop* index, so we look at idx2-1
+            new_end = max(end, self.clumps[idx2 - 1][1])
+
+            # 4. Atomic Replace
+            # Remove everything we touched and insert the one big merged result
+            self.clumps[idx1:idx2] = [(new_start, new_end)]
+            
+        else:
+            bisect.insort(self.clumps, (start, end))
         
         
         
